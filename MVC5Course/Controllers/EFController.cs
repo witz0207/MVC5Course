edf6e827aefec6786ab0e989f177f945fb10014b
witz0207 <loybiawitz@gmail.com>
2016-10-16 11:16:13 +0800

    temp

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -65,8 +65,6 @@ namespace MVC5Course.Controllers
         }
 
         // POST: Products/Edit/5
-        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
-        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "ProductId,ProductName,Price,Active,Stock")] Product product)
