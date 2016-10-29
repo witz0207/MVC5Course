f8093821bd0c1954a5210d229b834ad9049decdf
witz0207 <loybiawitz@gmail.com>
2016-10-29 14:26:58 +0800

    modified : 1.Practice other Actionresults 2.TempData Practice

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -1,17 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using MVC5Course.Models;
 
 namespace MVC5Course.Controllers
 {
     public class ARController : Controller
     {
+        private FabricsEntities db = new FabricsEntities();
+
         // GET: AR
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Notfound()
+        {
+            return View();
+        }
+
+        public ActionResult PartialViewTest()
+        {
+            return PartialView();
+        }
+
+        public ActionResult ContentTest()
+        {
+            return Content("ContentTest!!", "text/plain", Encoding.GetEncoding("Big5"));
+        }
+
+        public ActionResult FileTest()
+        {
+
+            var filePath = Server.MapPath("~/Content/view.jpg");
+
+            return File(filePath, "image/jpeg");
+        }
+
+        public ActionResult FileTest2()
+        {
+            var filePath = Server.MapPath("~/Content/view.jpg");
+
+            return File(filePath, "image/jpeg", "view.jpg");
+        }
+
+        public ActionResult JasonTest()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+            var data = db.Product.OrderBy(x => x.ProductId);
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
