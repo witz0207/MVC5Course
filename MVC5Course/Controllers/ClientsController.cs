bbe08724e7d6aef657e1dcb2bb5b4167cc594f6a
witz0207 <loybiawitz@gmail.com>
2016-10-29 16:32:25 +0800

    modified : Client Controller's Edit use "TryUpdateModel"

--- MVC5Course/Controllers/ClientsController.cs
+++ MVC5Course/Controllers/ClientsController.cs
@@ -96,14 +96,16 @@ namespace MVC5Course.Controllers
         // POST: Clients/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes")] Client client)
+        public ActionResult Edit(int id, FormCollection form)
         {
-            if (ModelState.IsValid)
+            var client = db.Client.Find(id);
+            if (TryUpdateModel(client, null, null, new string[] { "IsAdmin" }))
             {
-                db.Entry(client).State = EntityState.Modified;
                 db.SaveChanges();
+
                 return RedirectToAction("Index");
             }
+
             ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName", client.OccupationId);
             return View(client);
         }
