d57a4aa411c4e99d30ff1667dda5b328778b1fe9
witz0207 <loybiawitz@gmail.com>
2016-10-29 12:07:17 +0800

    modified : practice ARController to set "ViewEngines.Engines.Add(new RazorViewEngine());"

--- MVC5Course/Global.asax.cs
+++ MVC5Course/Global.asax.cs
@@ -18,6 +18,11 @@ namespace MVC5Course
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+            //ViewEngines.Engines.Add(new WebFoemViewEngine());
+
         }
     }
 }
