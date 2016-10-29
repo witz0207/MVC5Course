a080d241cfc74dc35965d0a15e6228a6cade10d5
witz0207 <loybiawitz@gmail.com>
2016-10-29 15:34:54 +0800

    modified: 1.Rename MVController to MBController 2.Modelbinding with batchupdate

---
+++ MVC5Course/Controllers/MBController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models.ViewModels;
+
+namespace MVC5Course.Controllers
+{
+    public class MBController : BaseController
+    {
+        // GET: MV
+        public ActionResult Index()
+        {
+            ViewData["Temp1"] = "test : ViewData";
+
+            var b = new ClientLoginViewModel()
+            {
+                FirstName = "Witz",
+                LastName = "Yu"
+            };
+
+            ViewData["Temp2"] = b;
+
+            ViewBag.Temp3 = b;
+
+            return View();
+        }
+
+        public ActionResult MyForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MyForm(ClientLoginViewModel c)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["MyFormData"] = c;
+                return RedirectToAction("MyFormResult");
+            }
+            return View();
+        }
+
+        public ActionResult MyFormResult()
+        {
+            return View();
+        }
+
+        public ActionResult ProductList()
+        {
+            var data = db.Product.OrderBy(p => p.ProductId).Take(10);
+            return View(data);
+        }
+
+        public ActionResult BatchUpdate(ProductBatchUpdateViewModel[] items)
+        {
+            /*
+             * 預設輸出的欄位名稱格式：item.ProductId
+             * 要改成以下欄位格式：
+             * items[0].ProductId
+             * items[1].ProductId
+             */
+            if (ModelState.IsValid)
+            {
+                foreach (var item in items)
+                {
+                    var product = db.Product.Find(item.ProductId);
+                    product.ProductName = item.ProductName;
+                    product.Active = item.Active;
+                    product.Stock = item.Stock;
+                    product.Price = item.Price;
+                }
+
+                db.SaveChanges();
+
+                return RedirectToAction("ProductList");
+            }
+
+            return View();
+        }
+    }
+}
