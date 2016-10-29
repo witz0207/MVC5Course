03a0d25c7e5b87f4cf28f344569974f9121c150b
witz0207 <loybiawitz@gmail.com>
2016-10-29 10:13:19 +0800

    modified : using Repository in ProductControllers

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -13,11 +13,17 @@ namespace MVC5Course.Controllers
     public class ProductsController : Controller
     {
         private FabricsEntities db = new FabricsEntities();
+        ProductRepository repo = RepositoryHelper.GetProductRepository();
+
 
         // GET: Products
         public ActionResult Index()
         {
-            return View(db.Product.OrderByDescending(p => p.ProductId).Take(10).ToList());
+            //return View(db.Product.OrderByDescending(p => p.ProductId).Take(10).ToList());
+
+            // change to use repository
+            var data = repo.All().OrderByDescending(p => p.ProductId).Take(10).ToList();
+            return View(data);
         }
 
         // GET: Products/Details/5
@@ -27,7 +33,11 @@ namespace MVC5Course.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Product product = db.Product.Find(id);
+
+            //Product product = db.Product.Find(id);
+            Product product = repo.Find(id.Value);
+
+
             if (product == null)
             {
                 return HttpNotFound();
@@ -50,6 +60,7 @@ namespace MVC5Course.Controllers
         {
             if (ModelState.IsValid)
             {
+                // write into DB no-need to put in repository
                 db.Product.Add(product);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -65,7 +76,10 @@ namespace MVC5Course.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Product product = db.Product.Find(id);
+
+            //Product product = db.Product.Find(id);
+            Product product = repo.Find(id.Value);
+
             if (product == null)
             {
                 return HttpNotFound();
@@ -74,16 +88,16 @@ namespace MVC5Course.Controllers
         }
 
         // POST: Products/Edit/5
-        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
-        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "ProductId,ProductName,Price,Active,Stock")] Product product)
         {
             if (ModelState.IsValid)
             {
+                // write into DB no-need to put in repository
                 db.Entry(product).State = EntityState.Modified;
                 db.SaveChanges();
+
                 return RedirectToAction("Index");
             }
             return View(product);
@@ -96,7 +110,10 @@ namespace MVC5Course.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Product product = db.Product.Find(id);
+            
+            //Product product = db.Product.Find(id);
+            Product product = repo.Find(id.Value);
+
             if (product == null)
             {
                 return HttpNotFound();
@@ -109,11 +126,15 @@ namespace MVC5Course.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Product product = db.Product.Find(id);
+            //Product product = db.Product.Find(id);
+            Product product = repo.Find(id);
+
             //db.Product.Remove(product);
             product.IsDeleted = true;
 
-            db.SaveChanges();
+            //db.SaveChanges();
+            repo.UnitOfWork.Commit();
+
             return RedirectToAction("Index");
         }
 
@@ -121,7 +142,8 @@ namespace MVC5Course.Controllers
         {
             if (disposing)
             {
-                db.Dispose();
+                //db.Dispose();
+                repo.UnitOfWork.Context.Dispose();
             }
             base.Dispose(disposing);
         }
--- MVC5Course/Models/ProductRepository.cs
+++ MVC5Course/Models/ProductRepository.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
-	
-namespace MVC5Course.Models
-{   
-	public  class ProductRepository : EFRepository<Product>, IProductRepository
-	{
 
-	}
+namespace MVC5Course.Models
+{
+
+
+    public class ProductRepository : EFRepository<Product>, IProductRepository
+	{
+        public Product Find(int id)
+        {
+            return All().FirstOrDefault(p => p.ProductId == id);
+        }
+    }
 
 	public  interface IProductRepository : IRepository<Product>
 	{
