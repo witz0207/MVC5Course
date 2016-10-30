bcb56fd825879266a3c41427cb961252b41bc2c8
witz0207 <loybiawitz@gmail.com>
2016-10-30 17:05:44 +0800

    practice : dropdownlist with "SelectList"

--- MVC5Course/Controllers/ClientsController.cs
+++ MVC5Course/Controllers/ClientsController.cs
@@ -16,7 +16,7 @@ namespace MVC5Course.Controllers
         private FabricsEntities db = new FabricsEntities();
 
         // GET: Clients
-        public ActionResult Index(string search)
+        public ActionResult Index(string search, int? CreditRating, string Gender)
         {
             var client = db.Client.Include(c => c.Occupation);
 
@@ -26,6 +26,11 @@ namespace MVC5Course.Controllers
             }
 
             client = client.OrderByDescending(p => p.ClientId).Take(10);
+
+            var options = (from p in db.Client select p.CreditRating).Distinct().OrderBy(p => p).ToList();
+            ViewBag.CreditRating = new SelectList(options);
+
+            ViewBag.Gender = new SelectList(new string[] { "M", "F" });
 
             return View(client);
         }
